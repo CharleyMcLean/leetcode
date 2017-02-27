6e2e09e38c7848d6509b3014d1d6ca75987e1cb7
CharleyMcLean <charleycodes@gmail.com>
2017-02-27 11:14:17 -0800

    Working on alternative solution using dictionaries. When num is in the list twice and 2*num == target, alternative solution is passing.  The other test case is not.

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -18,8 +18,50 @@ class Solution(object):
                 # index value of the entire list.
                 return [i, nums[(i + 1):].index(match) + i + 1]
 
+    def twoSumAlt(self, nums, target):
+        """
+        :type nums:List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Create a dictionary with each num from the original list as a key,
+        # and the compliment of that number as the value.  The compliment is
+        # the target number - the number.  Also create a dict to hold the counts
+        # of nums in case 2*num == target and num appears twice.
+        compliments = {}
+        counts = {}
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+            compliments[num] = [target-num]
+
+        # Create variables to hold the two index values
+        first_ind = int()
+        sec_ind = int()
+
+        # Check if the condition exists that num appears twice in list and
+        # 2*num == target
+        for num in counts:
+            if counts[num] == 2 and num * 2 == target:
+                first_ind = nums.index(num)
+                sec_ind = nums[(first_ind + 1):].index(num) + first_ind + 1
+
+        # If the above condition is not true, find the num that has the
+        # the compliment
+        for num in compliments:
+            if compliments[num] in nums:
+                first_ind = nums.index(num)
+                sec_ind = nums.index(compliments[num])
+                
+        return [first_ind, sec_ind]
+
+
+
 #########################################################################
 # Testing...
 solution = Solution()
 print "Expecting '[2, 3]', got...", solution.twoSum([1, 2, 3, 4], 7)
 print "Expecting '[0, 3]', got...", solution.twoSum([25, 2, 3, 25, 67, 8], 50)
+
+# testing the alternative function
+print "Expecting '[2, 3]', got...", solution.twoSumAlt([1, 2, 3, 4], 7)
+print "Expecting '[0, 3]', got...", solution.twoSumAlt([25, 2, 3, 25, 67, 8], 50)
